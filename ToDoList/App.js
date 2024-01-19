ec03f006c694f1104fb76ef77dd7a9325b110d21
BaNg-W <bang344251871@gmail.com>
2024-01-18 22:47:32 -0500

    Added ToDoList and AddTask

---
+++ ToDoList/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import ToDoList from './components/ToDoList.js'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <ToDoList initialTasks = {["Task 1", "Task 2", "Task 3"]} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
